07589319b3264171034133b38492d3648b343f89
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2024-11-22 12:44:59 -0500

    Parte 1 Finalizada

--- src/diagnostico/diagnostico.module.ts
+++ src/diagnostico/diagnostico.module.ts
@@ -1,7 +1,12 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
 import { DiagnosticoService } from './diagnostico.service';
+import { DiagnosticoEntity } from './diagnostico.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DiagnosticoEntity])],
   providers: [DiagnosticoService]
 })
 export class DiagnosticoModule {}
--- src/medico/medico.module.ts
+++ src/medico/medico.module.ts
@@ -3,8 +3,11 @@
 
 import { Module } from '@nestjs/common';
 import { MedicoService } from './medico.service';
+import { MedicoEntity } from './medico.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MedicoEntity])],
   providers: [MedicoService]
 })
 export class MedicoModule {}
