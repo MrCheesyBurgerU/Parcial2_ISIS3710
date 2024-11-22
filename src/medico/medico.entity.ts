fe07f717d89b7a9bad6e85f68d9f7d445521e5ce
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2024-11-22 08:16:06 -0500

    Persistencia Finalizada

---
+++ src/medico/medico.entity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+
+import { PacienteEntity } from "src/paciente/paciente.entity";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('medico')
+export class MedicoEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    especialidad: string;
+
+    @Column()
+    telefono: string;
+
+    @ManyToMany(() => PacienteEntity, paciente => paciente.medicos)
+    pacientes: PacienteEntity[];
+}
---
+++ src/paciente/paciente.entity.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+
+import { DiagnosticoEntity } from "src/diagnostico/diagnostico.entity";
+import { MedicoEntity } from "src/medico/medico.entity";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('paciente')
+export class PacienteEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    genero: string;
+
+    @ManyToMany(() => DiagnosticoEntity, diagnostico => diagnostico.pacientes)
+    @JoinTable()
+    diagnosticos: DiagnosticoEntity[];
+
+    @ManyToMany(() => MedicoEntity, medico => medico.pacientes)
+    @JoinTable()
+    medicos: MedicoEntity[];
+}
